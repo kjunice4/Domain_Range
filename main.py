f3fae23fc3672a4720b464e7d28566764dd40429
Kevin Junice <37306150+kjunice4@users.noreply.github.com>
2021-12-31 13:20:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.core.window import Window
 from kivy.garden.matplotlib.backend_kivyagg import FigureCanvasKivyAgg
+import matplotlib
 import matplotlib.pyplot as plt
 
 #Opening Page
